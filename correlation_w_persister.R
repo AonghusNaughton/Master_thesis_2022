934629551cf016e59d8bcfe5f96d0f3764d1a05a
AonghusNaughton <naughton@kth.se>
2022-04-21 10:19:21 +0200

    21 April

--- correlation_w_persister.R
+++ correlation_w_persister.R
@@ -19,79 +19,134 @@ for (i in names){
 }
 
 correlations <- lapply(scaled_counts, function(x){
-  res <- cor(x=as.matrix(t(x[rownames(x)[!rownames(x) %in% persister_genes],])),
-             y=as.matrix(t(x[rownames(x)[rownames(x) %in% persister_genes],])),
-             method = "spearman")
+  res <- correlatePairs(x, pairings = list(rownames(x)[!rownames(x) %in% persister_genes],
+                                           rownames(x)[rownames(x) %in% persister_genes]))
 })
 
-saveRDS(correlations, "correlations_allVsPersister.Rds")
-
-average_cors <- lapply(names, function(x){
-  y <- data.frame(rowMeans(correlations[[x]]))
-  colnames(y) <- paste0(x)
-  return(y)
-})
-
-names(average_cors) <- lapply(names, function(x) x)
-
-average_cors <- lapply(names(average_cors), function(x){
-  name <- paste0(x)
-  absent <- setdiff(highly_exp_genes, rownames(average_cors[[x]]))
-  new_rows <- lapply(absent, function(y){
-    df <- data.frame(y, 0)
-    rownames(df) <- df[[1]]
-    df[[1]] <- NULL
-    colnames(df) <- c(name)
-    return(df)
-  })
-  new_rows <- do.call(rbind, new_rows)
-  cors <- rbind(average_cors[[x]], new_rows)
-  return(cors)
-})
-
-names(average_cors) <- lapply(names, function(x) x)
-
-average_cors_df <- do.call(cbind, average_cors)
-
-MRD_cases <- readRDS("persister_cases.Rds")
-Relapse_cases <- readRDS("relapsed_cases.Rds")
-
-tidy <- average_cors_df %>%
-  as.matrix %>% as.table() %>% as.data.frame() %>%
-  rename(Gene=Var1, Patient=Var2, Average_correlation=Freq) %>%
-  mutate(MRD_or_Relapse=case_when(Patient %in% MRD_cases ~ "MRD",
-                                  Patient %in% Relapse_cases ~ "Relapse")) %>%
-  group_by(Gene) %>%
-  summarize(Mean_average_exp = mean(Average_correlation, na.rm=TRUE)) %>%
-  arrange(desc(Mean_average_exp))
-  
-ranks <- tidy$Mean_average_exp
-names(ranks) <- tidy$Gene
-ranks <- sort(ranks, decreasing = T)
-
-gse <- gseGO(geneList=ranks, 
-             ont ="BP", 
-             keyType = "SYMBOL", 
-             nPermSimple = 100000, 
-             minGSSize = 3, 
-             maxGSSize = 800, 
-             pvalueCutoff = 0.05, 
-             verbose = TRUE, 
-             OrgDb = org.Hs.eg.db, 
-             pAdjustMethod = "none")
-
-pdf("test.pdf",height = 12, width = 10)
-dotplot(gse, showCategory=20, split=".sign") + facet_grid(.~.sign) + scale_y_discrete(labels=function(x) str_wrap(x, width=55))
-dev.off()
-
-
-
-# Gene pairs 
-gene_pairs <- lapply(correlations, function(x){
+sig.corr <- lapply(correlations, function(x){
   x %>%
-    as.table() %>% as.data.frame() %>%
-    mutate(combined=paste(Var1, Var2, sep = "."))
+    as.data.frame() %>%
+    filter(FDR <= 0.05) %>%
+    filter(FDR != 0) %>%
+    mutate(combined=paste(gene1, gene2, sep = "."))
 })
+
+cmb <- combn(c(unique(dat.sub$patient_id)), 2)
+
+df_for_plot <- apply(cmb, 2, function(i){
+  x <- sig.corr[[i[1]]]
+  y <- sig.corr[[i[2]]]
+  common <- intersect(x$combined, y$combined)
+  dfx <- data.frame(x[x$combined %in% common,])
+  order <- dfx$combined
+  dfy <- data.frame(y[y$combined %in% common,])
+  dfy <- dfy %>%
+    arrange(factor(combined, levels=order))
+  df <- data.frame(dfx, dfy)
+  df <- df[, c("rho", "rho.1", "combined", "gene1", "gene2")]
+  return(df)
+})
+
+names(df_for_plot) <- apply(cmb, 2, function(x){
+  paste(x[1], x[2], sep = ".")
+})
+
+plots <- lapply(names(df_for_plot), function(x){
+  a <- gsub("\\..*", "", x)
+  b <- gsub(".*\\.", "", x)
+  ggplot(df_for_plot[[x]], aes(rho, rho.1, label=c(gene1, gene2))) +
+    geom_point() +
+    geom_text(size=3, check_overlap = TRUE, aes(color=)) +
+    geom_smooth(method = "lm", se = FALSE, col='red', size=1) +
+    xlab(paste("Gene pair correlations in ", a, sep = "")) +
+    ylab(paste("Gene pair correlations in ", b, sep = "")) +
+    theme_classic() +
+    geom_vline(xintercept = 0) +
+    geom_hline(yintercept = 0)
+})
+names(plots) <- lapply(names(df_for_plot), function(x) x)
+
+for (i in names(plots)){
+  ggsave(filename = paste("/Users/aonghusnaughton/Proj_eng/April22/int_cor_structure/persister_and_relapse/AllvsPersister/", i, ".pdf", sep = ""),
+         plot = plots[[i]],
+         width = 8,
+         height = 5)
+}
+
+# correlations <- lapply(scaled_counts, function(x){
+#   res <- cor(x=as.matrix(t(x[rownames(x)[!rownames(x) %in% persister_genes],])),
+#              y=as.matrix(t(x[rownames(x)[rownames(x) %in% persister_genes],])),
+#              method = "spearman")
+# })
+# 
+# saveRDS(correlations, "correlations_allVsPersister.Rds")
+# 
+# average_cors <- lapply(names, function(x){
+#   y <- data.frame(rowMeans(correlations[[x]]))
+#   colnames(y) <- paste0(x)
+#   return(y)
+# })
+# 
+# names(average_cors) <- lapply(names, function(x) x)
+# 
+# average_cors <- lapply(names(average_cors), function(x){
+#   name <- paste0(x)
+#   absent <- setdiff(highly_exp_genes, rownames(average_cors[[x]]))
+#   new_rows <- lapply(absent, function(y){
+#     df <- data.frame(y, 0)
+#     rownames(df) <- df[[1]]
+#     df[[1]] <- NULL
+#     colnames(df) <- c(name)
+#     return(df)
+#   })
+#   new_rows <- do.call(rbind, new_rows)
+#   cors <- rbind(average_cors[[x]], new_rows)
+#   return(cors)
+# })
+# 
+# names(average_cors) <- lapply(names, function(x) x)
+# 
+# average_cors_df <- do.call(cbind, average_cors)
+# 
+# MRD_cases <- readRDS("persister_cases.Rds")
+# Relapse_cases <- readRDS("relapsed_cases.Rds")
+# 
+# tidy <- average_cors_df %>%
+#   as.matrix %>% as.table() %>% as.data.frame() %>%
+#   rename(Gene=Var1, Patient=Var2, Average_correlation=Freq) %>%
+#   mutate(MRD_or_Relapse=case_when(Patient %in% MRD_cases ~ "MRD",
+#                                   Patient %in% Relapse_cases ~ "Relapse")) %>%
+#   group_by(Gene) %>%
+#   summarize(Mean_average_exp = mean(Average_correlation, na.rm=TRUE)) %>%
+#   arrange(desc(Mean_average_exp))
+#   
+# ranks <- tidy$Mean_average_exp
+# names(ranks) <- tidy$Gene
+# ranks <- sort(ranks, decreasing = T)
+# 
+# gse <- gseGO(geneList=ranks, 
+#              ont ="BP", 
+#              keyType = "SYMBOL", 
+#              nPermSimple = 100000, 
+#              minGSSize = 3, 
+#              maxGSSize = 800, 
+#              pvalueCutoff = 0.05, 
+#              verbose = TRUE, 
+#              OrgDb = org.Hs.eg.db, 
+#              pAdjustMethod = "none")
+# 
+# pdf("test.pdf",height = 12, width = 10)
+# dotplot(gse, showCategory=20, split=".sign") + facet_grid(.~.sign) + scale_y_discrete(labels=function(x) str_wrap(x, width=55))
+# dev.off()
+# 
+# 
+# 
+# # Gene pairs 
+# gene_pairs <- lapply(correlations, function(x){
+#   x %>%
+#     as.table() %>% as.data.frame() %>%
+#     mutate(combined=paste(Var1, Var2, sep = "."))
+# })
 
 
 
--- internal_cor_structure.r
+++ internal_cor_structure.r
@@ -6,7 +6,7 @@ names <- unique(dat.sub$patient_id)
 
 persister_genes <- readRDS("persister_genes.Rds")
 relapse_genes <- readRDS("relapse_genes.Rds")
-genes <- c(persister_genes, relapse_genes)
+genes <- unique(c(persister_genes, relapse_genes))
 
 df1 <- lapply(names, function(x){
   if (x=="ALL3"){
@@ -48,70 +48,135 @@ for (i in names){
   df2[[i]] <- df2[[i]][gene_list_for_scaled[[i]],]
 }
 
-corr <- lapply(df2, function(x){
-  res <- corr.test(as.matrix(t(x)), method = "spearman")
+
+var.corr <- lapply(df2, function(x){
+  res <- correlatePairs(x)
 })
 
-res <- lapply(corr, function(x){
-  x$p %>%
-    {(function(x){x[lower.tri(x)] <- NA; x})(.)} %>%
-    as.table() %>% as.data.frame() %>%
-    subset(Var1 != Var2) %>%
-    subset(!is.na(Freq)) %>%
-    subset(Freq < 0.05) %>%
-    mutate(combined=paste(Var1, Var2, sep = ".")) %>%
-    mutate(genes=case_when(Var1 %in% persister_genes & Var2 %in% persister_genes ~ "Persister pair",
-                             (Var1 %in% persister_genes & Var2 %in% relapse_genes |
-                                Var1 %in% relapse_genes & Var2 %in% persister_genes) ~ "Mix pair",
-                             Var1 %in% relapse_genes & Var2 %in% relapse_genes ~ "Relapse pair")) %>%
-    dplyr::select(-c(Var1, Var2)) %>%
-    arrange(combined)
-  x$r %>%
-    
+sig.corr <- lapply(var.corr, function(x){
+  x %>%
+    as.data.frame() %>%
+    filter(FDR <= 0.05) %>%
+    filter(FDR != 0)
+  # res <- as.data.frame(x[x$FDR <= 0.05,])
+})
+
+sig.corr <- lapply(sig.corr, function(x){
+  x %>%
+    mutate(gene_set=case_when(gene1 %in% persister_genes & gene2 %in% persister_genes ~ "Persister genes",
+                              gene1 %in% persister_genes & gene2 %in% relapse_genes ~ "Mixed pair",
+                              gene2 %in% persister_genes & gene1 %in% relapse_genes ~ "Mixed pair",
+                              gene1 %in% relapse_genes & gene2 %in% relapse_genes ~ "Relapse genes")) %>%
+    mutate(combined=paste(gene1, gene2, sep = "."))
 })
 
 cmb <- combn(c(unique(dat.sub$patient_id)), 2)
 
 df_for_plot <- apply(cmb, 2, function(i){
-  x <- res[[i[1]]]
-  y <- res[[i[2]]]
+  x <- sig.corr[[i[1]]]
+  y <- sig.corr[[i[2]]]
   common <- intersect(x$combined, y$combined)
-  df <- data.frame(x[x$combined %in% common,], y[y$combined %in% common,])
-  df <- df[, c("Freq", "Freq.1", "combined", "genes")]
+  dfx <- data.frame(x[x$combined %in% common,])
+  order <- dfx$combined
+  dfy <- data.frame(y[y$combined %in% common,])
+  dfy <- dfy %>%
+    arrange(factor(combined, levels=order))
+  df <- data.frame(dfx, dfy)
+  df <- df[, c("rho", "rho.1", "combined", "gene_set")]
   return(df)
 })
-
 
 names(df_for_plot) <- apply(cmb, 2, function(x){
   paste(x[1], x[2], sep = ".")
 })
 
-
-filtered <- lapply(df_for_plot, function(x){
-  x %>%
-    filter((Freq > 0.1 & Freq.1 > 0.1) | (Freq < -0.1 & Freq.1 < -0.1))
-})
-
-plots <- lapply(names(filtered), function(x){
-  a <- gsub("\\..*", "", x)
-  b <- gsub(".*\\.", "", x)
-  ggplot(filtered[[x]], aes(Freq, Freq.1, color=genes)) +
-    geom_point() +
-    geom_smooth(method = "lm", se = FALSE, col='red', size=1) +
-    xlab(paste("Gene pair correlations in ", a, sep = "")) +
-    ylab(paste("Gene pair correlations in ", b, sep = "")) +
-    theme_classic() +
-    geom_vline(xintercept = 0) +
-    geom_hline(yintercept = 0)
-})
-
-names(plots) <- lapply(names(test), function(x) x)
+plots <- lapply(names(df_for_plot), function(x){
+    a <- gsub("\\..*", "", x)
+    b <- gsub(".*\\.", "", x)
+    ggplot(df_for_plot[[x]], aes(rho, rho.1, color=gene_set)) +
+      geom_point() +
+      geom_smooth(method = "lm", se = FALSE, col='red', size=1) +
+      xlab(paste("Gene pair correlations in ", a, sep = "")) +
+      ylab(paste("Gene pair correlations in ", b, sep = "")) +
+      theme_classic() +
+      geom_vline(xintercept = 0) +
+      geom_hline(yintercept = 0)
+  })
 
 for (i in names(plots)){
-  ggsave(filename = paste("/Users/aonghusnaughton/Proj_eng/April22/int_cor_structure/persister_and_relapse/filtered/", i, ".pdf", sep = ""),
+  ggsave(filename = paste("/Users/aonghusnaughton/Proj_eng/April22/int_cor_structure/persister_and_relapse/p_val_filtered/", i, ".pdf", sep = ""),
          plot = plots[[i]],
          width = 8,
          height = 5)
 }
+
+# corr <- lapply(df2, function(x){
+#   res <- corr.test(as.matrix(t(x)), method = "spearman")
+# })
+# 
+# res <- lapply(corr, function(x){
+#   x$p %>%
+#     {(function(x){x[lower.tri(x)] <- NA; x})(.)} %>%
+#     as.table() %>% as.data.frame() %>%
+#     subset(Var1 != Var2) %>%
+#     subset(!is.na(Freq)) %>%
+#     subset(Freq < 0.05) %>%
+#     mutate(combined=paste(Var1, Var2, sep = "."),
+#            combined_backwards=paste(Var2, Var1, sep = "."))
+#   
+#   x$r %>%
+#     as.table() %>% as.data.frame() %>%
+#     mutate(combined=paste(Var1, Var2, sep = ".")) %>%
+#     subset((combined  %in% a$combined | combined %in% a$combined_backwards)) %>%
+#     mutate(geneSet=case_when(Var1 %in% persister_genes & Var2 %in% persister_genes ~ "Persister",
+#                              Var1 %in% persister_genes & Var2 %in% relapse_genes ~ "Relapse-Persister",
+#                              Var1 %in% relapse_genes & Var2 %in% persister_genes ~ "Persister-Relapse",
+#                              Var1 %in% relapse_genes & Var2 %in% relapse_genes ~ "Relapse"))
+#     
+# })
+# 
+# cmb <- combn(c(unique(dat.sub$patient_id)), 2)
+# 
+# df_for_plot <- apply(cmb, 2, function(i){
+#   x <- res[[i[1]]]
+#   y <- res[[i[2]]]
+#   common <- intersect(x$combined, y$combined)
+#   df <- data.frame(x[x$combined %in% common,], y[y$combined %in% common,])
+#   df <- df[, c("Freq", "Freq.1", "combined", "genes")]
+#   return(df)
+# })
+# 
+# 
+# names(df_for_plot) <- apply(cmb, 2, function(x){
+#   paste(x[1], x[2], sep = ".")
+# })
+# 
+# 
+# filtered <- lapply(df_for_plot, function(x){
+#   x %>%
+#     filter((Freq > 0.1 & Freq.1 > 0.1) | (Freq < -0.1 & Freq.1 < -0.1))
+# })
+# 
+# plots <- lapply(names(filtered), function(x){
+#   a <- gsub("\\..*", "", x)
+#   b <- gsub(".*\\.", "", x)
+#   ggplot(filtered[[x]], aes(Freq, Freq.1, color=genes)) +
+#     geom_point() +
+#     geom_smooth(method = "lm", se = FALSE, col='red', size=1) +
+#     xlab(paste("Gene pair correlations in ", a, sep = "")) +
+#     ylab(paste("Gene pair correlations in ", b, sep = "")) +
+#     theme_classic() +
+#     geom_vline(xintercept = 0) +
+#     geom_hline(yintercept = 0)
+# })
+# 
+names(plots) <- lapply(names(df_for_plot), function(x) x)
+# 
+# for (i in names(plots)){
+#   ggsave(filename = paste("/Users/aonghusnaughton/Proj_eng/April22/int_cor_structure/persister_and_relapse/filtered/", i, ".pdf", sep = ""),
+#          plot = plots[[i]],
+#          width = 8,
+#          height = 5)
+# }
 
 
